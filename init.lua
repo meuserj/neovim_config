9bbbb66286e31d9933139aa771484656504f7509
John Meuser <meuserj@gmail.com>
2024-10-24 12:21:00 -0400

    Add Aerial

--- init.lua
+++ init.lua
@@ -8,6 +8,16 @@ require("lspconfig").eslint.setup({})
 require("lspconfig").jsonls.setup({})
 require("lspconfig").marksman.setup({})
 require("lspconfig").pug.setup({})
+require("aerial").setup({
+  -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+  on_attach = function(bufnr)
+    -- Jump forwards/backwards with '{' and '}'
+    vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+    vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+  end,
+})
+-- You probably also want to set a keymap to toggle aerial
+vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 local bufferline = require("bufferline")
 bufferline.setup({
   options = {
--- lua/plugins/enabled.lua
+++ lua/plugins/enabled.lua
@@ -20,4 +20,13 @@ return {
       require("alpha").setup(require("alpha.themes.theta").config)
     end,
   },
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+       "nvim-treesitter/nvim-treesitter",
+       "nvim-tree/nvim-web-devicons"
+    },
+  }
 }
