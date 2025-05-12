2a37f77300334a32a9bd16b860c3c1cc3a08f00f
John Meuser <meuserj@gmail.com>
2025-05-12 10:53:43 -0400

    Do not load many plugins when running in vscode

--- init.lua
+++ init.lua
@@ -2,12 +2,12 @@
 require("config.lazy")
 
 vim.o.background = "dark"
-vim.cmd([[colorscheme gruvbox]])
 
-require("lspconfig").eslint.setup({})
-require("lspconfig").jsonls.setup({})
-require("lspconfig").marksman.setup({})
 if not vim.g.vscode then
+  require("lspconfig").eslint.setup({})
+  require("lspconfig").jsonls.setup({})
+  require("lspconfig").marksman.setup({})
+  vim.cmd([[colorscheme gruvbox]])
   require("aerial").setup({
     -- optionally use on_attach to set keymaps when aerial has attached to a buffer
     on_attach = function(bufnr)
@@ -26,49 +26,48 @@ if not vim.g.vscode then
       kind = "floating",
     },
   })
-end
+  require("lualine").setup({
+    options = {
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+    },
+  })
+  local bufferline = require("bufferline")
+  bufferline.setup({
+    options = {
+      numbers = "buffer_id",
+      separator_style = "slant",
+      diagnostics = "nvim_lsp",
+      custom_areas = {
+        right = function()
+          local result = {}
+          local seve = vim.diagnostic.severity
+          local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+          local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+          local info = #vim.diagnostic.get(0, { severity = seve.INFO })
+          local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
 
-require("lualine").setup({
-  options = {
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-  },
-})
+          if error ~= 0 then
+            table.insert(result, { text = "  " .. error, link = "DiagnosticError" })
+          end
+
+          if warning ~= 0 then
+            table.insert(result, { text = "  " .. warning, link = "DiagnosticWarn" })
+          end
+
+          if hint ~= 0 then
+            table.insert(result, { text = "  " .. hint, link = "DiagnosticHint" })
+          end
+
+          if info ~= 0 then
+            table.insert(result, { text = "  " .. info, link = "DiagnosticInfo" })
+          end
+          return result
+        end,
+      },
+    },
+  })
+end
 
 -- You probably also want to set a keymap to toggle aerial
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
-local bufferline = require("bufferline")
-bufferline.setup({
-  options = {
-    numbers = "buffer_id",
-    separator_style = "slant",
-    diagnostics = "nvim_lsp",
-    custom_areas = {
-      right = function()
-        local result = {}
-        local seve = vim.diagnostic.severity
-        local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
-        local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
-        local info = #vim.diagnostic.get(0, { severity = seve.INFO })
-        local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
-
-        if error ~= 0 then
-          table.insert(result, { text = "  " .. error, link = "DiagnosticError" })
-        end
-
-        if warning ~= 0 then
-          table.insert(result, { text = "  " .. warning, link = "DiagnosticWarn" })
-        end
-
-        if hint ~= 0 then
-          table.insert(result, { text = "  " .. hint, link = "DiagnosticHint" })
-        end
-
-        if info ~= 0 then
-          table.insert(result, { text = "  " .. info, link = "DiagnosticInfo" })
-        end
-        return result
-      end,
-    },
-  },
-})
