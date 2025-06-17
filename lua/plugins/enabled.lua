4fa0c91dfaa268449a0b1e53ddc4e18d282e207a
John Meuser <meuserj@gmail.com>
2025-06-17 13:02:24 -0400

    Switch to Deepseek

--- lua/plugins/enabled.lua
+++ lua/plugins/enabled.lua
@@ -51,7 +51,7 @@ return {
             },
             schema = {
               model = {
-                default = 'gemma3:27b',
+                default = 'deepseek-r1:latest',
               },
             },
           })
