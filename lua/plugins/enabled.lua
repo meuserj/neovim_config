fb2d10eb3dafd9c7af7f4004bb8ad665a13515ad
John Meuser <meuserj@gmail.com>
2025-06-30 11:51:15 -0400

    Switch to Avante

--- lua/plugins/enabled.lua
+++ lua/plugins/enabled.lua
@@ -1,6 +1,12 @@
 return {
   { "OXY2DEV/helpview.nvim" },
-  { "OXY2DEV/markview.nvim" },
+  { "OXY2DEV/markview.nvim",
+    opts = {
+      experimental = {
+        check_rtp = false,
+      },
+    },
+  },
   { "danro/rename.vim" },
   { "junegunn/vim-easy-align" },
   { "vim-scripts/bufonly.vim" },
@@ -29,38 +35,58 @@ return {
     config = true,
   },
   {
-    "olimorris/codecompanion.nvim",
+    "yetone/avante.nvim",
+    build = function()
+      if vim.fn.has("win32") == 1 then
+        return "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      else
+        return "make"
+      end
+    end,
+    event = "VeryLazy",
     opts = {
-      strategies = {
-        chat = {
-          adapter = 'ollama',
-        },
-        inline = {
-          adapter = 'ollama',
-        },
-        agent = {
-          adapter = 'ollama',
+      provider = "ollama",
+      providers = {
+        ollama = {
+          endpoint = "http://127.0.0.1:11434",
+          model = "codegemma",
         },
       },
-      adapters = {
-        ollama = function()
-          return require('codecompanion.adapters').extend('ollama', {
-            name = 'local_ollama',
-            env = {
-              url = 'http://localhost:11434',
-            },
-            schema = {
-              model = {
-                default = 'gemma3:27b',
-              },
-            },
-          })
-        end,
-      },
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
     },
   },
+  -- {
+  --   "olimorris/codecompanion.nvim",
+  --   opts = {
+  --     strategies = {
+  --       chat = {
+  --         adapter = 'ollama',
+  --       },
+  --       inline = {
+  --         adapter = 'ollama',
+  --       },
+  --       agent = {
+  --         adapter = 'ollama',
+  --       },
+  --     },
+  --     adapters = {
+  --       ollama = function()
+  --         return require('codecompanion.adapters').extend('ollama', {
+  --           name = 'local_ollama',
+  --           env = {
+  --             url = 'http://localhost:11434',
+  --           },
+  --           schema = {
+  --             model = {
+  --               default = 'gemma3:27b',
+  --             },
+  --           },
+  --         })
+  --       end,
+  --     },
+  --   },
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+  -- },
 }
