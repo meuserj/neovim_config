1e290d90c6abbf77e65775af94084f7ca8830168
John Meuser <john.meuser@genesys.com>
2026-05-18 09:21:13 -0400

    Switch back to Carlos's Agentic and switch to Cursor

--- lua/plugins/agentic.lua
+++ lua/plugins/agentic.lua
@@ -13,33 +13,19 @@ return {
       function() require("agentic").switch_provider() end,
       desc = "Switch Agentic Provider",
       mode = "n",
-    },
-    { "<leader>ar", function() require("agentic").restore_session() end, desc = "Agentic: Restore Session" },
-    { "<leader>an", function() require("agentic").new_session() end, desc = "Agentic: New Session" },
+    }
   },
   config = function()
     local profile = os.getenv("NVIM_PROFILE") or "home"
-    
     -- At home we use "gemini". At work, we pretend to use "opencode-acp"
     local active_provider = "gemini-acp"
     if profile == "work" then
-      active_provider = "opencode-acp"
+      active_provider = "cursor-acp"
     end
 
     require("agentic").setup({
       provider = active_provider,
       auto_add_to_context = true,
-      
-      acp_providers = {
-        -- Hijack the opencode-acp slot to run Kiro instead!
-        ["opencode-acp"] = {
-          command = "kiro-cli",
-          args = { "acp" },
-          env = {
-            KIRO_LOG_LEVEL = "info",
-          },
-        },
-      },
     })
   end,
 }
